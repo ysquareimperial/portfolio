86fb8a6a701bef0e4737512760406f3a7cf9803f
ysquareimperial <ysquare.theimperial@gmail.com>
2025-06-03 20:53:41 +0100

    save

--- src/Portfolio/HomePage.jsx
+++ src/Portfolio/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Portfolio.css";
-import yasir from "../images/yasss.png";
+import yasir from "../images/yasir.png";
 import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
